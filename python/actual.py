701a331b82dff2bc0d86b3a04d09f325c8c42deb
Cody Lent <lentcody@yahoo.com>
2017-11-27 20:11:14 -0500

    updated the naughty list!

--- python/actual.py
+++ python/actual.py
@@ -5,7 +5,14 @@ from sys import stdin, argv
 from os.path import basename
 
 naughty_list = ["FT: Do not re-install same PTK to the driver", 
-                "oh, another one"]
+                "WPA: Failed to get random data for ANonce",
+                "TDLS: TPK-TK for the peer", 
+                "has already been configrued to the driver - do not reconfigure",
+                "WNM: Ignore WNM-Sleep Mode Response frame since WNM-Sleep Mode operation has not been requested",
+                "WPA: Not reinstalling already in-use GTK to the driver",
+                "FT: Reassociation has already been completed for this FT protocol instance - ignore unexpected retransmission",
+                "CodyLentPatch3Placeholder",
+                "CodyLentPatch4Placeholder"]
 
 def main(command):
     if command == "":
@@ -18,13 +25,15 @@ def main(command):
         if any(naughty_phrase in line for naughty_phrase in naughty_list):
             #Found a bad printout
             print "Found a bad one!"
-        else:
-            print "Read good line"
+            
 if __name__ == "__main__":
     file_name = basename(__file__)
     command = ""
     for token in argv:
         if token != file_name:
             command += token + " "
+            
+    if "-ddd" not in command:
+        command += "-ddd"
 
     main(command)
