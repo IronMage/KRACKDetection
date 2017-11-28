3743089c1fa6dd3fcf9db454603e50b8474b505a
jonepl <jonepl.ufl@gmail.com>
2017-11-28 17:24:34 -0500

    Added method that will soon be used to evaluate input streams of wpa_supplicant lines. Working on changing the naughty list array to a dictionary for easier identification of CVEs

--- python/actual.py
+++ python/actual.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env pyt
 
 import subprocess
-import re
-import os.path
-from sys import stdin, argv
+from sys import stdin, argv, stdout
 from os.path import basename
+
+nl = {"FT: Do not re-install same PTK to the driver" : "CVE-2017-#",
+      "WPA: Failed to get random data for ANonce" : "CVE-2017-#",
+      "TDLS: TPK-TK for the peer" : "CVE-2017-#",
+      "has already been configrued to the driver - do not reconfigure" : "CVE-2017-#",
+      "WNM: Ignore WNM-Sleep Mode Response frame since WNM-Sleep Mode operation has not been requested" : "CVE-2017-#",
+      "WPA: Not reinstalling already in-use GTK to the driver" : "CVE-2017-#",
+      "FT: Reassociation has already been completed for this FT protocol instance - ignore unexpected retransmission" : "CVE-2017-#",
+      "CodyLentPatch3Placeholder" : "CVE-2017-#",
+      "CodyLentPatch4Placeholder" : "CVE-2017-#",
+      "play10_4WayMsg4" : "CVE-2017-#"}
 
 naughty_list = ["FT: Do not re-install same PTK to the driver",
                 "WPA: Failed to get random data for ANonce",
@@ -16,6 +25,18 @@ naughty_list = ["FT: Do not re-install same PTK to the driver",
                 "CodyLentPatch3Placeholder",
                 "CodyLentPatch4Placeholder",
                 "play10_4WayMsg4"]
+
+def evaluate_file(command):
+    pass;
+
+def evaluate(command):
+    pass
+
+def post_results(result):
+    #
+    pass
+
+
 
 def main(command):
     print "Your command:", command
@@ -39,16 +60,16 @@ def main(command):
                 proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
                 for line in iter(f.readline, ''):
                     print "."
-                    if any(naughty_phrase in line for naughty_phrase in naughty_list):
+                    if any(naughty_phrase in line for naughty_phrase in nl):
                         #Found a bad printout
-                        print "Found a bad one!"
+                        print "\nFound a bad one!"
 
         if(not found) :
             print "Command must contain \"test filename.ext\" to test file for naughty phrases."
     else:
         proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
         for line in iter(proc.stdout.readline, ''):
-            if any(naughty_phrase in line for naughty_phrase in naughty_list):
+            if any(naughty_phrase in line for naughty_phrase in nl):
                 #Found a bad printout
                 print "Found a bad one!"
 
