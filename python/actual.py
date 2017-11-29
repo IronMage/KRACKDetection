be60f51f21773e3154359da465625315ed1a53c3
jonepl <jonepl.ufl@gmail.com>
2017-11-29 12:22:56 -0500

    Added functionality to determine whether vulnerable or not vulnerable

--- python/actual.py
+++ python/actual.py
@@ -4,29 +4,33 @@ import subprocess
 from sys import stdin, argv, stdout
 from os.path import basename
 
-naughty_list = {"FT: Do not re-install same PTK to the driver" : "CVE-2017-#",
-      "WPA: Failed to get random data for ANonce" : "CVE-2017-#",
-      "TDLS: TPK-TK for the peer" : "CVE-2017-#",
-      "has already been configrued to the driver - do not reconfigure" : "CVE-2017-#",
-      "WNM: Ignore WNM-Sleep Mode Response frame since WNM-Sleep Mode operation has not been requested" : "CVE-2017-#",
-      "WPA: Not reinstalling already in-use GTK to the driver" : "CVE-2017-#",
-      "FT: Reassociation has already been completed for this FT protocol instance - ignore unexpected retransmission" : "CVE-2017-#",
-      "CodyLentPatch3Placeholder" : "CVE-2017-#",
-      "CodyLentPatch4Placeholder" : "CVE-2017-#",
-      "play10_4WayMsg4" : "CVE-2017-#"}
-"""
-naughty_list = ["FT: Do not re-install same PTK to the driver",
-                "WPA: Failed to get random data for ANonce",
-                "TDLS: TPK-TK for the peer",
-                "has already been configrued to the driver - do not reconfigure",
-                "WNM: Ignore WNM-Sleep Mode Response frame since WNM-Sleep Mode operation has not been requested",
-                "WPA: Not reinstalling already in-use GTK to the driver",
-                "FT: Reassociation has already been completed for this FT protocol instance - ignore unexpected retransmission",
-                "CodyLentPatch3Placeholder",
-                "CodyLentPatch4Placeholder",
-                "play10_4WayMsg4"]
-"""
+nice_list = {"FT: Do not re-install same PTK to the driver" : "CVE-2017-13077", #0001
+      "WPA: Not reinstalling already in-use IGTK to the driver" : "CVE-2017-13081", #0002
+      "WPA: Invalid IGTK KeyID" : "CVE-2017-13081", #0002
+      "Failed to configure IGTK to the driver" : "CVE-2017-13081", #0002
+      "WPA: Failed to get random data for ANonce" : "CVE-2017-", #0005
+      "WPA: Assign new ANonce" : "CVE-2017-", #0005
+      "TDLS: TPK-TK for the peer" : "CVE-2017-13086", #0006
+      "TDLS: Configure pairwise key for peer" : "CVE-2017-", #0006
+      "has already been configrued to the driver - do not reconfigure" : "CVE-2017-#", #0006
+      "WNM: Ignore WNM-Sleep Mode Response frame since WNM-Sleep Mode operation has not been requested" : "CVE-2017-13088", #0007
+      "FT: Reassociation has already been completed for this FT protocol instance - ignore unexpected retransmission" : "CVE-2017-13082", #0008
+      }
+naughty_list = [
+    "WPA: Failed to configure IGTK to the driver", #0002
+    "WPA: IGTK keyid %d pn %02x%02x%02x%02x%02x%02x", #0002
+    "Install IGTK (WNM SLEEP)", # 0002
+    "Failed to install the IGTK in WNM mode", #0002
+    "WNM: Ignore WNM-Sleep Mode Response frame since WNM-Sleep Mode has not been used in this association", #0007
+    ]
 
+def printResults(vl, nvl):
+    print "\nVulnerable List:"
+    for item in vl :
+        print item;
+    print "\nNot vulnerable List:"
+    for item in nvl :
+        print item;
 
 
 def main(command):
@@ -36,13 +40,26 @@ def main(command):
         print "Must supply wpa_supplicant command"
         return 0
 
+    vulnerable = [];
+    not_vulnerable = [];
+
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+
     for line in iter(proc.stdout.readline, ''):
+        for msg in nice_list:
+            if msg in line:
+                #Found a bad printout
+                #print nice_list[msg]
+                not_vulnerable.append(nice_list[msg]);
+                break
+
         for msg in naughty_list:
             if msg in line:
                 #Found a bad printout
-                print naughty_list[msg]
+                #print msg
+                vulnerable.append(msg);
                 break
+    printResults(vulnerable, not_vulnerable);
 
 if __name__ == "__main__":
     file_name = basename(__file__)
